531720a825896d12ab2562f75b26448f6eca61bc
Devin Cenatiempo <dcenatiempo@gmail.com>
2018-05-31 16:56:40 -0600

    fix

--- util/dbConnect.php
+++ util/dbConnect.php
@@ -1,7 +1,7 @@
 <?php
 
 $dbUrl = getenv('DATABASE_URL');
-$dbUrl = "postgres://vqmvgjpetvbkfr:6db2b56e64cb6fc32737a410acc91e1e3048689c197a496ec44ae0013b61c112@ec2-23-23-130-158.compute-1.amazonaws.com:5432/d4522ubjgvhe7p";
+
 $dbopts = parse_url($dbUrl);
 
 $dbHost = $dbopts["host"];
