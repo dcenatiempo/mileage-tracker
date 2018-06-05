fb1638106389ccff115c45a9c25472325d80ebd7
Devin Cenatiempo <dcenatiempo@gmail.com>
2018-06-05 10:26:02 -0600

    fix

--- views/dashboard.php
+++ views/dashboard.php
@@ -1,7 +1,7 @@
 <?php
 require_once("../views/head.php");
 require_once("../views/meta.php");
-  echo "<h1>Dashboard<h1>";
+  echo "<h1>Dashboard</h1>";
   require_once("../views/vehicleMileageList.php");
   require_once("../views/addVehicle.php");
   require_once("../views/changeVehicle.php");
